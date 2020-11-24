06115397fcbdb83ce915e726cfb6be145d02a34d
Brett Needham <brettneedham88@gmail.com>
2020-11-24 18:40:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,13 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  data.each_with_object({}) do |(k, v), a|
+  data.each_with_object({}) do |(key, value), final_array|
+    value.each do |inner_key, names|
+      names.each do |name|
+        if !final_array[name]
+          final_array[name] = {}
+        end
+        if !final_array[name][key] = []
+        end
   binding.pry
 end
